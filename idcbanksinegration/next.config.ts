f348249d75a8705accd784f1a7bb818648b7ec19
Brighton13 <brightonbanda13@gmail.com>
2026-06-04 15:20:04 +0200

    qwertyu

--- idcbanksinegration/next.config.ts
+++ idcbanksinegration/next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  allowedDevOrigins: ['10.51.60.220', 'localhost', '127.0.0.1'],
+  // You can also use wildcards if needed:
+  // allowedDevOrigins: ['*.your-domain.com', '10.51.60.*'],
 };
 
 export default nextConfig;
